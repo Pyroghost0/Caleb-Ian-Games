5fbefbb73c44722fb83490df5752e8cce1acffc4
Pyroghost0 <89740009+Pyroghost0@users.noreply.github.com>
2022-06-04 15:46:41 -0500

    Added 3rd Person Controller & Player That Stays Across Levels

---
+++ Character Creator Jam/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject player;
+    public string loadedPlayerScene = "Player";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+        {
+            Debug.Log("Loading player...");
+            AsyncOperation ao = SceneManager.LoadSceneAsync(loadedPlayerScene, LoadSceneMode.Additive);
+            if (ao == null)
+            {
+                Debug.LogError("Unable to load player " + loadedPlayerScene);
+            }
+        }
+    }
+}
---
+++ Character Creator Jam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public CharacterController controller;
+    public float speed = 12f;
+    public Vector3 velocity;
+    public float gravity = -9.8f;
+    public float gravityMultiplier = 2f;
+
+    public Transform groundCheck;
+    public float groundDistence = .4f;
+    public LayerMask groundMask;
+    public bool isGrounded;
+    public float jumpHeight = 3f;
+
+    public float mouseHorizontalSensitivity = 400f;
+    public float mouseVirticalSensitivity = 100f;
+    public float minAngle = -10f;
+    public float maxAngle = 30f;
+    private float verticalLookRotation = 0f;
+    public GameObject cameraBasisObject;
+
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("Player Manager").GetComponent<PlayerManager>().player = gameObject;
+    }
+
+    void Awake()
+    {
+        gravity *= gravityMultiplier;
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseHorizontalSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseVirticalSensitivity * Time.deltaTime;
+        transform.Rotate(Vector3.up * mouseX);
+        verticalLookRotation -= mouseY;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, minAngle, maxAngle);//Cant over rotate
+        cameraBasisObject.transform.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);//apply clamp
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistence, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
+
