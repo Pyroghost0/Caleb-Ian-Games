aa60d81657bf0cff5d5cb7514118eacd79617950
Pyroghost0 <89740009+Pyroghost0@users.noreply.github.com>
2022-06-19 19:02:32 -0500

    Zip

--- Character Creator Jam/Assets/Scripts/MainScreenManager.cs
+++ Character Creator Jam/Assets/Scripts/MainScreenManager.cs
@@ -7,7 +7,16 @@ public class MainScreenManager : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Tutorial");
+        //SceneManager.LoadScene("Tutorial");
+        StartCoroutine(WaitLoad());
+    }
+
+    IEnumerator WaitLoad()
+    {
+        AsyncOperation ao1 = SceneManager.LoadSceneAsync("Player", LoadSceneMode.Additive);
+        AsyncOperation ao2 = SceneManager.LoadSceneAsync("Deer Level", LoadSceneMode.Additive);
+        yield return new WaitUntil(() => ao1.isDone && ao2.isDone);
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName("Main Screen"));
     }
 
     public void ExitGame()
