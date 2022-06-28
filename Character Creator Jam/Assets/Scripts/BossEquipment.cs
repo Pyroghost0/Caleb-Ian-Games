cc7464303ccd59e84bc9abc1beddbad89276344a
AbstractThink81 <89855045+AbstractThink81@users.noreply.github.com>
2022-06-28 00:43:00 -0500

    Boss picks equipment based on player

---
+++ Character Creator Jam/Assets/Scripts/BossEquipment.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEquipment : MonoBehaviour
+{
+    public bool isMale = false;
+    public GameObject[] maleObjects;
+    public GameObject[] femaleObjects;
+    public GameObject[] selfEquipment;
+    public int equipedSet; //0 = mech, 1 = deer, 2 = baker, 3 = potus
+
+    private PlayerStatus playerStatus;
+    private int offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerStatus = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStatus>();
+        if (playerStatus.isMale)
+		{
+            isMale = false;
+            for (int i = 0; i < femaleObjects.Length; i++)
+			{
+                femaleObjects[i].SetActive(true);
+                maleObjects[i].SetActive(false);
+            }
+		}
+		else
+		{
+            isMale = true;
+            for (int i = 0; i < femaleObjects.Length; i++)
+            {
+                femaleObjects[i].SetActive(false);
+                maleObjects[i].SetActive(true);
+            }
+        }
+        offset = 0;
+        if (isMale) offset = 12;
+
+        if (playerStatus.equipedEquipment[0] == false && playerStatus.equipedEquipment[1] == false && playerStatus.equipedEquipment[2] == false)
+		{
+            equipedSet = 0;
+            selfEquipment[0 + offset].SetActive(true);
+            selfEquipment[1 + offset].SetActive(true);
+            selfEquipment[2 + offset].SetActive(true);
+
+		}
+		else if (playerStatus.equipedEquipment[3] == false && playerStatus.equipedEquipment[4] == false && playerStatus.equipedEquipment[5] == false)
+		{
+            equipedSet = 1;
+            selfEquipment[3 + offset].SetActive(true);
+            selfEquipment[4 + offset].SetActive(true);
+            selfEquipment[5 + offset].SetActive(true);
+        }
+        else if (playerStatus.equipedEquipment[6] == false && playerStatus.equipedEquipment[7] == false && playerStatus.equipedEquipment[8] == false)
+        {
+            equipedSet = 2;
+            selfEquipment[6 + offset].SetActive(true);
+            selfEquipment[7 + offset].SetActive(true);
+            selfEquipment[8 + offset].SetActive(true);
+        }
+		else
+		{
+            equipedSet = 3;
+            selfEquipment[9 + offset].SetActive(true);
+            selfEquipment[10 + offset].SetActive(true);
+            selfEquipment[11 + offset].SetActive(true);
+            maleObjects[0].SetActive(false);
+            femaleObjects[0].SetActive(false);
+        }
+    }
+}
