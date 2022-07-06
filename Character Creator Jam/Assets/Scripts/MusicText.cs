825efb6e14d3e4045fb187d02e092777e6f61835
Pyroghost0 <89740009+Pyroghost0@users.noreply.github.com>
2022-07-06 18:11:26 -0500

    Sliders Done

---
+++ Character Creator Jam/Assets/Scripts/MusicText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MusicText : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    private PlayerMovement player;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<TextMeshProUGUI>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    public void UpdateText()
+    {
+        text.text = slider.value.ToString();
+        player.autoAimPower = slider.value * 100f;
+    }
+}
